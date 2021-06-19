6c17ee6c7de1347ea8786c5dd1cca55bfaac46fd
DRQSuperior <74627000+DRQSuperior@users.noreply.github.com>
2021-06-19 02:58:49 -0700

    SKIDDED
    SKIDDED

---
+++ Anti Cheat Source/Fortnite/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Fortnite
+{
+	// Token: 0x02000002 RID: 2
+	internal class Program
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002048 File Offset: 0x00000248
+		private static void Main(string[] args)
+		{
+			Console.Title = "FrostNite Hybrid";
+			string str = string.Empty;
+			string text = string.Empty;
+			foreach (string text2 in args)
+			{
+				bool flag = text2.Contains("-epicusername=");
+				bool flag2 = flag;
+				bool flag3 = flag2;
+				bool flag4 = flag3;
+				if (flag4)
+				{
+					str = text2.Replace("-epicusername=", "");
+				}
+				text = text + text2 + " ";
+			}
+			Console.WriteLine("Loading FrostNite" + str);
+			Console.WriteLine("Finding A Slot At FrostNite Servers");
+			Console.WriteLine("Slot Found!");
+			Console.WriteLine("FrostNite Has Been Loaded");
+			Process.Start("FortniteClient-Win64-Shipping.exe", text);
+			Process[] processes = Process.GetProcesses();
+			foreach (Process process in processes)
+			{
+				bool flag5 = process.ProcessName.Contains("FortniteClient-Win64-Shipping");
+				bool flag6 = flag5;
+				bool flag7 = flag6;
+				bool flag8 = flag7;
+				if (flag8)
+				{
+					Console.WriteLine("FrostNite Has Been Launched");
+					break;
+				}
+			}
+			Console.Read();
+		}
+	}
+}
---
+++ Anti Cheat Source/Fortnite/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Fortnite Anti-Cheat Disabler")]
+[assembly: AssemblyDescription("Creado Por Robin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fortnite")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5c9b7dd8-d3e1-43b2-beda-37bf5e4443a3")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
